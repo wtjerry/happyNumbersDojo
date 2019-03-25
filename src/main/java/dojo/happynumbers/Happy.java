96118a087e249b9d525117406dffcfb6d09a0848
Alan Meile <alan.meile@stud.hslu.ch>
2019-03-25 16:28:46 +0100

    wire Happy.validate with its submodules

--- src/main/java/dojo/happynumbers/Happy.java
+++ src/main/java/dojo/happynumbers/Happy.java
@@ -1,18 +1,23 @@
 package dojo.happynumbers;
 
-
 import java.util.Collections;
 
 class Happy {
 
+    private final DigitSplitter digitSplitter;
     private final SquareAndSum squareAndSum;
+    private final LoopDetector loopDetector;
 
-    Happy(SquareAndSum squareAndSum) {
+    Happy(DigitSplitter digitSplitter, SquareAndSum squareAndSum, LoopDetector loopDetector) {
+        this.digitSplitter = digitSplitter;
         this.squareAndSum = squareAndSum;
+        this.loopDetector = loopDetector;
     }
 
-    boolean validate(final int i) {
-        this.squareAndSum.squareAndSum(Collections.singletonList(i));
+    boolean validate(final int number) {
+        var digits = this.digitSplitter.split(number);
+        int squareAndSum = this.squareAndSum.squareAndSum(digits);
+        this.loopDetector.isLoop(Collections.emptyList(), squareAndSum);
         return true;
     }
 }
--- src/test/java/dojo/happynumbers/HappyTest.java
+++ src/test/java/dojo/happynumbers/HappyTest.java
@@ -4,18 +4,69 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class HappyTest {
 
     @Test
-    void does2ResultInSquareSumBeingCalled() {
+    void validateCallsSubmodulesInCorrectOrder() {
+        var digitSplitterMock = mock(DigitSplitter.class);
         var squareMock = mock(SquareAndSum.class);
-        var happy = new Happy(squareMock);
+        var loopDetectorMock = mock(LoopDetector.class);
+        var happy = new Happy(digitSplitterMock, squareMock, loopDetectorMock);
 
-        happy.validate(2);
+        var digits = Collections.singletonList(7);
+        var squareAndSum = 49;
 
-        verify(squareMock).squareAndSum(Collections.singletonList(2));
+        when(digitSplitterMock.split(3)).thenReturn(digits);
+        when(squareMock.squareAndSum(digits)).thenReturn(squareAndSum);
+
+        happy.validate(3);
+
+        verify(loopDetectorMock).isLoop(Collections.emptyList(), squareAndSum);
+    }
+
+    @Test
+    void validateCallsDigitSplitter() {
+        var digitSplitterMock = mock(DigitSplitter.class);
+        var happy = new Happy(digitSplitterMock, mock(SquareAndSum.class), mock(LoopDetector.class));
+
+        happy.validate(3);
+
+        verify(digitSplitterMock).split(3);
+    }
+
+    @Test
+    void validateCallsSquareAndSum() {
+        var digitSplitterMock = mock(DigitSplitter.class);
+        var squareMock = mock(SquareAndSum.class);
+        var happy = new Happy(digitSplitterMock, squareMock, mock(LoopDetector.class));
+
+        var digits = Collections.singletonList(7);
+
+        when(digitSplitterMock.split(anyInt())).thenReturn(digits);
+
+        happy.validate(3);
+
+        verify(squareMock).squareAndSum(digits);
+    }
+
+    @Test
+    void validateCallsLoopDetector() {
+        var digitSplitterMock = mock(DigitSplitter.class);
+        var squareMock = mock(SquareAndSum.class);
+        var loopDetectorMock = mock(LoopDetector.class);
+        var happy = new Happy(digitSplitterMock, squareMock, loopDetectorMock);
+
+        var digits = Collections.singletonList(7);
+        var squareAndSum = 49;
+
+
+        when(digitSplitterMock.split(anyInt())).thenReturn(digits);
+        when(squareMock.squareAndSum(any())).thenReturn(squareAndSum);
+
+        happy.validate(3);
+
+        verify(loopDetectorMock).isLoop(Collections.emptyList(), squareAndSum);
     }
 }
